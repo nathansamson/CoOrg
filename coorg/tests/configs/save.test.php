19096637999fce80f2114217d3fe08df25063707
Nathan Samson <nathan@gamma>
2010-06-20 22:33:44 +0200

    Config can save array keys

--- coorg/tests/configs/save.test.php
+++ coorg/tests/configs/save.test.php
@@ -1,3 +1,6 @@
 <?php
-$_config['myChangedKey'] = '...';$_config['myUnChangedKey'] = 'myUnChangedValue';$_config['myArrayValue'] = array('Google\'s best browser
-');$_config['myStrangeValue'] = 'é & " \\ () <?php shit; ?> \\\'';?>
+
+$_config['myChangedKey'] = '...';
+$_config['myUnChangedKey'] = 'myUnChangedValue';
+
+?>
