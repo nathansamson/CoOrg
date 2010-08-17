d5976f2b6b44190d7794eec0c045cf2bbce9b53b
Nathan Samson <nathan@gamma>
2010-08-18 01:17:53 +0200

    Remove flattr.js from static file

--- plugins/flattr/static/default/resources.coorg.php
+++ plugins/flattr/static/default/resources.coorg.php
@@ -1,6 +1,5 @@
 <?php
 
-CoOrg::resreg('flattr', 'flattr.js', '12345678901234567890123456789012');
 CoOrg::resreg('flattr', 'flattr-default.png', '12345678901234567890123456789012');
 CoOrg::resreg('flattr', 'flattr-compact.png', '12345678901234567890123456789012');
 
